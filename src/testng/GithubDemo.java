3e75e69307b6aa409fa06611c8bf7ca4a40666f1
karimbedu <kpurushotham.777@gmail.com>
2022-11-10 11:34:15 +0530

    Updated Gitdem class

--- src/testng/GithubDemo.java
+++ src/testng/GithubDemo.java
@@ -2,11 +2,13 @@ package testng;
 
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeTest;
+import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterTest;
 
 public class GithubDemo {
+	WebDriver driver;
   @Test
-  public void f() {
+  public void gitdemo() {
   }
   @BeforeTest
   public void beforeTest() {
